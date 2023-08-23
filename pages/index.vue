c9e20ff8f3f6082039c758d7cada212e7c819bc3
Mark Teekens <mark@rubyfinance.nl>
2023-08-23 18:02:41 +0200

    feature: add products from index difference

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,16 @@
 definePageMeta({
   layout: "default",
 });
+
+const { data, error, refresh } = await useAsyncData("getProducts", () =>
+  $fetch("/api/getProducts", {
+    method: "POST",
+    body: {
+      cartItems: [],
+    },
+  })
+);
+const products = data.value?.data;
 </script>
 
 <template>
@@ -18,9 +28,22 @@ definePageMeta({
         class="absolute lg:h-60 h-40 z-10 w-full bg-gradient-to-t from-white/30 opacity-100 to-transparent"
       ></div>
     </div>
-    <div class="flex flex-col items-center min-h-screen">
-      <div class="grid md:grid-cols-2 grid-cols-1 mt-0.5 md:gap-6 md:mt-6">
-        <collection-block name="office" image="/collectionimages/office.jpg" />
+    <div class="flex flex-col items-center mt-6">
+      <div
+        class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6"
+      >
+        <div
+          class="flex relative items-center justify-center h-80 w-80"
+          :key="i"
+          v-for="(item, i) in products"
+        >
+          <product-block
+            :name="(item.name as string)"
+            :image="(item.image as string)"
+            :id="item.id"
+            :price="(item.price as number)"
+          />
+        </div>
       </div>
     </div>
   </div>
